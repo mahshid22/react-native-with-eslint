2a53ab41e649e71b95014890d80df3e48ffbab61
mahshid22 <mahshid.pouyafar@gmail.com>
2024-08-07 23:34:33 +0430

    add some usefull rule to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 
 export default [
-  {files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']},
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   {
     settings: {
       react: {
@@ -16,19 +16,87 @@ export default [
   },
   {
     rules: {
-      "no-console": "error",
-      "react/prop-types": "error",
-      "react/react-in-jsx-scope": "error",
-      "@typescript-eslint/no-unused-vars": "error",
-      "@typescript-eslint/no-explicit-any": "error",
-      "prefer-const": "error",
-      "@typescript-eslint/no-var-requires": "off",
-      "no-console": "error",
-      "no-alert": "error",
-      "no-debugger": "error"
-    }
+      'no-console': 'error',
+      'react/prop-types': 'error',
+      'react/react-in-jsx-scope': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      'prefer-const': 'error',
+      '@typescript-eslint/no-var-requires': 'off',
+      'no-console': 'error',
+      'no-alert': 'error',
+      'no-debugger': 'error',
+      'react/prefer-stateless-function': 'error',
+      'react/button-has-type': 'error',
+      'react/no-unused-prop-types': 'error',
+      'react/jsx-pascal-case': 'error',
+      'react/jsx-no-script-url': 'error',
+      'react/no-children-prop': 'error',
+      'react/no-danger': 'error',
+      'react/no-danger-with-children': 'error',
+      'react/no-unstable-nested-components': ['error', { allowAsProps: true }],
+      'react/jsx-fragments': 'error',
+      'react/destructuring-assignment': ['error', 'always', { destructureInSignature: 'always' }],
+      'react/jsx-no-leaked-render': ['error', { validStrategies: ['ternary'] }],
+      'react/jsx-max-depth': ['error', { max: 5 }],
+      'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
+      'react/jsx-key': [
+        'error',
+        {
+          checkFragmentShorthand: true,
+          checkKeyMustBeforeSpread: true,
+          warnOnDuplicates: true,
+        },
+      ],
+      'react/jsx-no-useless-fragment': 'warn',
+      'react/jsx-curly-brace-presence': 'warn',
+      'react/no-typos': 'warn',
+      'react/display-name': 'warn',
+      'react/self-closing-comp': 'warn',
+      'react/jsx-sort-props': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-one-expression-per-line': 'off',
+      'react/prop-types': 'off',
+      "react/no-array-index-key": "error",
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        {
+          selector: "default",
+          format: ["camelCase"],
+          leadingUnderscore: "allow",
+        },
+        {
+          selector: "variable",
+          // Specify PascalCase for React components
+          format: ["PascalCase", "camelCase"],
+          leadingUnderscore: "allow",
+        },
+        {
+          selector: "parameter",
+          format: ["camelCase"],
+          leadingUnderscore: "allow",
+        },
+        {
+          selector: "property",
+          format: null,
+          leadingUnderscore: "allow",
+        },
+        {
+          selector: "typeLike",
+          format: ["PascalCase"],
+        },
+      ],
+    
+      "arrow-body-style": "warn",
+      "prefer-arrow-callback": [
+        "warn",
+        {
+          allowNamedFunctions: true,
+        },
+      ],
+    },
   },
-  {languageOptions: {globals: globals.browser}},
+  { languageOptions: { globals: globals.browser } },
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
