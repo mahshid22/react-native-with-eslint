53742308e00e36251c492e9d917f9143ca319a7f
mahshid22 <mahshid.pouyafar@gmail.com>
2024-08-08 17:29:27 +0430

    add some rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,13 @@ import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+  },
+  { ignores: ['**/*.config.js', '!**/eslint.config.js', '**/node_modules/', '.git/'] },
   {
     settings: {
       react: {
@@ -15,6 +21,10 @@ export default [
     },
   },
   {
+    linterOptions: {
+      noInlineConfig: true,
+      reportUnusedDisableDirectives: "error"
+    },
     rules: {
       'no-console': 'error',
       'react/prop-types': 'error',
@@ -39,7 +49,7 @@ export default [
       'react/destructuring-assignment': ['error', 'always', { destructureInSignature: 'always' }],
       'react/jsx-no-leaked-render': ['error', { validStrategies: ['ternary'] }],
       'react/jsx-max-depth': ['error', { max: 5 }],
-      'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
+      'react/function-component-definition': ['error', { namedComponents: 'arrow-function' }],
       'react/jsx-key': [
         'error',
         {
@@ -57,39 +67,39 @@ export default [
       'react/react-in-jsx-scope': 'off',
       'react/jsx-one-expression-per-line': 'off',
       'react/prop-types': 'off',
-      "react/no-array-index-key": "error",
-      "@typescript-eslint/naming-convention": [
-        "warn",
+      'react/no-array-index-key': 'error',
+      '@typescript-eslint/naming-convention': [
+        'warn',
         {
-          selector: "default",
-          format: ["camelCase"],
-          leadingUnderscore: "allow",
+          selector: 'default',
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
         },
         {
-          selector: "variable",
+          selector: 'variable',
           // Specify PascalCase for React components
-          format: ["PascalCase", "camelCase"],
-          leadingUnderscore: "allow",
+          format: ['PascalCase', 'camelCase'],
+          leadingUnderscore: 'allow',
         },
         {
-          selector: "parameter",
-          format: ["camelCase"],
-          leadingUnderscore: "allow",
+          selector: 'parameter',
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
         },
         {
-          selector: "property",
+          selector: 'property',
           format: null,
-          leadingUnderscore: "allow",
+          leadingUnderscore: 'allow',
         },
         {
-          selector: "typeLike",
-          format: ["PascalCase"],
+          selector: 'typeLike',
+          format: ['PascalCase'],
         },
       ],
-    
-      "arrow-body-style": "warn",
-      "prefer-arrow-callback": [
-        "warn",
+
+      'arrow-body-style': 'warn',
+      'prefer-arrow-callback': [
+        'warn',
         {
           allowNamedFunctions: true,
         },
